cf3fedc94eb7e0456db6b8a104d7594ad84b73d0
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-06-05 23:55:56 +0530

    creating react frontend app

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+
+//importing App component
+import App from './App'
+
+// importing css file
+import './index.css'
+
+ReactDOM.render(<App/>,document.getElementById('root'))
