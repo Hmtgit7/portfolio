cb5d5d5fd17900b30a883fd5cf1e43ab918d8d25
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-06-12 02:06:49 +0530

    initially commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.scss'
+
+import {About,Footer,Header,Skills,Testimonials,Work } from './containers';
+
+import {Navbar} from './Components';
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonials/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
