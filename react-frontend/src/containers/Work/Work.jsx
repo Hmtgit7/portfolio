8830e7a1dbba91caf0ee332c3d33fe6c485ba92c
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-06-25 17:02:39 +0530

    implementing component work

--- react-frontend/src/containers/Work/Work.jsx
+++ react-frontend/src/containers/Work/Work.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import "./Work.scss";
+
+// import {urlFor,client} from '../../client'
+import AppWrap from "../../Wrapper";
 
 const Work = () => {
-  return (
-    <div>Work</div>
-  )
-}
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
-export default Work
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>PortFolio </span>Section
+      </h2>
+      <div className="app__work-filter">
+        {["UI/UX", "Web-App", "React.js", "Mobile-App", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        
+      </motion.div>
+    </>
+  );
+};
+
+export default Work;
