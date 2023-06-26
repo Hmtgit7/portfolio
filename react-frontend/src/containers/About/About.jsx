31729b84c67da9e9fdb1a336c94183bc116eb74d
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-06-26 23:57:52 +0530

    Wrapping up the component for icon

--- react-frontend/src/containers/About/About.jsx
+++ react-frontend/src/containers/About/About.jsx
@@ -1,53 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-import { images } from "../../constants";
-
-import "./About.scss";
-
-const abouts = [
-  {
-    title: "Web Developer",
-    description: "Iam a good web developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Full-stack Developer",
-    description: "Iam a good web developer",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Mern Developer",
-    description: "Iam a good web developer",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Mean Developer",
-    description: "Iam a good web developer",
-    imgUrl: images.about04,
-  },
-];
+import { AppWrap, MotionWrap } from '../../Wrapper';
+import './About.scss'
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
-      <h2 className="head-text">I know That<span>Good Design </span><br />means<span> Good Business </span></h2>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
             key={about.title + index}
-            className="app__profile-items"
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 10 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
@@ -55,4 +39,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
