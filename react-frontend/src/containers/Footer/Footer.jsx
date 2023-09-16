23bc570dfa7a9808f90a68a50842a5d44a7eec75
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-16 19:45:37 +0530

    footer component

--- react-frontend/src/containers/Footer/Footer.jsx
+++ react-frontend/src/containers/Footer/Footer.jsx
@@ -1,86 +1,32 @@
-import React, { useState } from 'react';
-
-import { images } from '../../constants';
-import { AppWrap, MotionWrap } from '../../Wrapper';
-import { client } from '../../client';
-import './Footer.scss';
+import React from 'react'
+import { BsTwitter, BsInstagram, BsGithub, BsYoutube, BsLinkedin } from 'react-icons/bs';
+import './Footer.scss'
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const { username, email, message } = formData;
-
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = () => {
-    setLoading(true);
-
-    const contact = {
-      _type: 'contact',
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    client.create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
-    <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
-
-      <div className="app__footer-cards">
-        <div className="app__footer-card ">
-          <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+    <div className="footer">
+      <div className="icons">
+        <div>
+          <BsGithub />
         </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+        <div>
+          <BsInstagram />
+        </div>
+        <div>
+          <BsLinkedin />
+        </div>
+        <div>
+          <BsTwitter />
+        </div>
+        <div>
+          <BsYoutube />
         </div>
       </div>
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-          </div>
-          <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-          </div>
-          <div>
-            <textarea
-              className="p-text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
-          </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-        </div>
-      ) : (
-        <div>
-          <h3 className="head-text">
-            Thank you for getting in touch!
-          </h3>
-        </div>
-      )}
-    </>
-  );
-};
+      <div className="content">
+        <h1>Made with❤️ <a href=''>Spark Developie</a> Copyright @2023</h1>
+      </div>
+    </div>
+  )
+}
 
-export default AppWrap(
-  MotionWrap(Footer, 'app__footer'),
-  'contact',
-  'app__whitebg',
-);
+export default Footer
