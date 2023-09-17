8ea725d302cd4fa659e53949efc83ad9dd3ae63e
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-17 09:45:08 +0530

    modify

--- react-frontend/src/containers/About/About.jsx
+++ react-frontend/src/containers/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
       <div className="app__profiles">
         <About1 />
-        <h1>Services</h1> 
+        {/* <h1>Services</h1>  */}
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
