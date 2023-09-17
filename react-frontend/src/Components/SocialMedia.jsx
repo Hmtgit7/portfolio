9c5d4b5e346a1950f50e24a448d652a54e33b526
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-17 14:09:29 +0530

    adding links

--- react-frontend/src/Components/SocialMedia.jsx
+++ react-frontend/src/Components/SocialMedia.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BsTwitter, BsInstagram, BsGithub, BsYoutube, BsLinkedin } from 'react-icons/bs';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div >
-      <BsTwitter />
-    </div>
-    <div>
-      <BsGithub />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
-    <div>
-      <BsLinkedin />
-    </div>
+    <a href='https://www.linkedin.com/in/hemant-gehlod'>
+      <div>
+        <BsLinkedin />
+      </div>
+    </a>
+    <a href='https://www.github.com/Hmtgit7'>
+      <div>
+        <BsGithub />
+      </div>
+    </a>
+    <a href='https://twitter.com/GehlodHemant?t=xQzzJBMs9qf1b2Pyo-OLOA&s=09'>
+      <div >
+        <BsTwitter />
+      </div>
+    </a>
   </div>
 );
 
