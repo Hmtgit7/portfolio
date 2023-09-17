8fd64bfd92efb9fe055cd556bc4f84faa52b3875
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-09-17 14:10:50 +0530

    updating typewriter

--- react-frontend/src/containers/About1/About1.jsx
+++ react-frontend/src/containers/About1/About1.jsx
@@ -15,8 +15,8 @@ const About1 = () => {
   });
 
   const [text1] = useTypewriter({
-    words: ['Full Stack Developer', 'Mean Stack Developer', 'Mern Stack Developer'],
-    // words: ['Developer', 'Designer'],
+    // words: ['Full Stack Developer', 'Mean Stack Developer', 'Mern Stack Developer'],
+    words: ['Web Developer', 'Freelancer', 'Full Stack Developer'],
     loop: {}
   });
 
@@ -30,7 +30,10 @@ const About1 = () => {
           <span> {text}</span>
           <span><Cursor /></span>
         </h1>
-        <p>I am {" "} <span>{text1}.</span><br />I am final year Student. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur amet sequi, vel quod iure explicabo perferendis non voluptate consectetur voluptates!</p>
+        <p>I am {" "} <span>{text1}.</span><br />I am final year Student. Pursuing B.tech in Computer Science and Engineering from Acropolis institute of Technology and Reesearch, Indore, Madhya Pradesh, India.<br/>
+        Mean/Mern Stack Developer , Java Full Stack Developer
+        </p>
+        
         <button>Download CV</button>
       </div>
 
