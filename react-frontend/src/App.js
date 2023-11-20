402081cd384ab1a7c7fdfbb6b59e87c5260659b5
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-11-20 17:33:23 +0530

    initiating portfolio section

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <Navbar />
       <Header />
       <About />
+      <Portfolio/>
       <Skills />
       <Work />
       {/* <Testimonials/> */}
--- react-frontend/src/containers/Portfolio/Portfolio.jsx
+++ react-frontend/src/containers/Portfolio/Portfolio.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../Wrapper';
+import { urlFor, client } from '../../client';
+import './Portfolio.scss';
+import { BsFillArrowRightSquareFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
 
 const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
-}
 
-export default Portfolio
+  return (
+    <>
+     
+    </>
+  );
+};
+// export default AppWrap(
+//   MotionWrap(Work, 'app__work'),
+//   'work',
+//   'app__whitebg',
+// );
+export default AppWrap(Portfolio, 'portfolio')
