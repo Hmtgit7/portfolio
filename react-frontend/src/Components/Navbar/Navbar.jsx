e8b673870b8a4727122b0065b76cbbff61bd6e6f
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-08 21:31:37 +0530

    adding icons

--- react-frontend/src/Components/Navbar/Navbar.jsx
+++ react-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import {
+  BsTwitter,
+  BsInstagram,
+  BsGithub,
+  BsYoutube,
+  BsLinkedin,
+} from "react-icons/bs";
 
 // import { images } from '../../constants';
-import './Navbar.scss';
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -17,25 +24,51 @@ const Navbar = () => {
         </h1>
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'skills','work',  'contact'].map((item) => (
+        {["home", "about", "skills", "work", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
-
+      <div className="icons">
+        <a href="https://github.com/Hmtgit7">
+          <div>
+            <BsGithub />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/hemant_gehlod/" target="blank">
+          <div>
+            <BsInstagram />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/hemant-gehlod">
+          <div>
+            <BsLinkedin />
+          </div>
+        </a>
+        <a href="https://twitter.com/GehlodHemant?t=xQzzJBMs9qf1b2Pyo-OLOA&s=09">
+          <div>
+            <BsTwitter />
+          </div>
+        </a>
+        <a href='"https://youtube.com/channel/UCmAmylLKHr73VTcqJXLSMlA"'>
+          <div>
+            <BsYoutube />
+          </div>
+        </a>
+      </div>
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about',  'skills','work', 'contact'].map((item) => (
+              {["home", "about", "skills", "work", "contact"].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
