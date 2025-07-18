753868924d42a9d3cfe92c9acc763456de4bcc2b
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-07-18 14:51:12 +0530

    feat: add project images and update projectsImg.js with new assets

---
+++ react-frontend/src/constants/projectsImg.js
@@ -0,0 +1,22 @@
+import periskope from '../assets/projects/image.png';
+import tagmaster from '../assets/projects/tagmaster.png';
+import syncsphere from '../assets/projects/syncsphere.png';
+import shopyfi from '../assets/projects/shopyfi.png';
+import aiCareerCoach from '../assets/projects/ai-career-coach.png';
+import employeeManagement from '../assets/projects/employee-management.png';
+import bitly from '../assets/projects/bitly-clone.png';
+import realtimeSpotify from '../assets/projects/spotify-clone.png';
+import threadsClone from '../assets/projects/threads-clone.png';
+
+
+export default {
+    periskope,
+    tagmaster,
+    syncsphere,
+    shopyfi,
+    aiCareerCoach,
+    employeeManagement,
+    bitly,
+    realtimeSpotify,
+    threadsClone
+}
