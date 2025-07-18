016df2bfe0a293ac7d2412ac5ba3b15ce221af23
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-07-18 16:23:11 +0530

    fix: update CV download link in About1 component

--- react-frontend/src/containers/About1/About1.jsx
+++ react-frontend/src/containers/About1/About1.jsx
@@ -35,7 +35,7 @@ const About1 = () => {
         Mean/Mern Stack Developer , Java Full Stack Developer
         </p>
         
-        <a href="https://drive.google.com/file/d/1NFvU6eu0ZxX1l9CEYVy9gKbqaKAHhe5h/view?usp=sharing" target='_blank'><button className='p-text'>Download CV</button></a>
+        <a href="https://drive.google.com/file/d/1QWfi6aRyP4ZdL6Q6BMiGyUO9SYUb0gFj/view?usp=sharing" target='_blank'><button className='p-text'>Download CV</button></a>
       </div>
 
     </div>
