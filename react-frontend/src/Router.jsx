f927f8b2380c88e2bfab61c2028225c3e7a1fa98
Hmtgit7 <hmtloharcoding3579@gmail.com>
2026-06-10 00:56:01 +0530

    feat(router): add /projects/hirelyx and /projects/prajna detail page routes

--- react-frontend/src/Router.jsx
+++ react-frontend/src/Router.jsx
@@ -3,54 +3,49 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import HomePage from './HomePage';
 import Projects from './containers/Projects/Projects';
+import HirelyxPage from './containers/ProjectDetail/pages/HirelyxPage';
+import PrajnaPage from './containers/ProjectDetail/pages/PrajnaPage';
 import { PageTransition, ScrollToTop } from './Components';
 
-
-
-// Animated routes wrapper
 const AnimatedRoutes = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <AnimatePresence mode="wait">
-            <ScrollToTop />
-            <Routes location={location} key={location.pathname}>
-                <Route
-                    path="/"
-                    element={
-                        <PageTransition>
-                            <HomePage />
-                        </PageTransition>
-                    }
-                />
-                <Route
-                    path="/projects"
-                    element={
-                        <PageTransition>
-                            <Projects />
-                        </PageTransition>
-                    }
-                />
-                {/* Add a catch-all route that redirects to home */}
-                <Route
-                    path="*"
-                    element={
-                        <PageTransition>
-                            <HomePage />
-                        </PageTransition>
-                    }
-                />
-            </Routes>
-        </AnimatePresence>
-    );
+  return (
+    <AnimatePresence mode="wait">
+      <ScrollToTop />
+      <Routes location={location} key={location.pathname}>
+
+        <Route path="/" element={
+          <PageTransition><HomePage /></PageTransition>
+        } />
+
+        <Route path="/projects" element={
+          <PageTransition><Projects /></PageTransition>
+        } />
+
+        {/* Flagship project detail pages */}
+        <Route path="/projects/hirelyx" element={
+          <PageTransition><HirelyxPage /></PageTransition>
+        } />
+
+        <Route path="/projects/prajna" element={
+          <PageTransition><PrajnaPage /></PageTransition>
+        } />
+
+        {/* Catch-all → home */}
+        <Route path="*" element={
+          <PageTransition><HomePage /></PageTransition>
+        } />
+
+      </Routes>
+    </AnimatePresence>
+  );
 };
 
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <AnimatedRoutes />
-        </BrowserRouter>
-    );
-};
+const Router = () => (
+  <BrowserRouter>
+    <AnimatedRoutes />
+  </BrowserRouter>
+);
 
 export default Router;
